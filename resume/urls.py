955760a1da94269a34ebe6fbb1e7e3daeb93244d
Nuraiym Obozkanova <nuraika.obozkanova@gmail.com>
2023-03-21 19:33:50 +0600

    first commit

---
+++ resume/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from resume.views import index
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+	path("", index),
+	# path("/com", components),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
