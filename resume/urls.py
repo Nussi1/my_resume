a3712ac016fa4f7473451a0be7d4adec80afc420
Nuraika Obozkanova <nuraika.obozkanova@gmail.com>
2024-07-08 16:33:14 +0600

    contact added

--- resume/urls.py
+++ resume/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-from resume.views import index
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+
+from resume.views import index
+from .views import contact_view
 
 urlpatterns = [
-	path("", index),
-	# path("/com", components),
+ path("", index),
+ path('contact/', contact_view, name='contact'),
+ # path("/com", components),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- resume/views.py
+++ resume/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
 
 def index(request):
-	return render(request, template_name='index.html')
+    return render(request, template_name='index.html')
 
-#
-# def components(request):
-# 	return render(request, template_name='components.html')
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent successfully!')
+            return redirect('contact')
+    else:
+        form = ContactForm()
+    return render(request, 'index.html', {'form': form})
+
